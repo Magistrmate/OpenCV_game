5aede3978df9136c6db7e228a5c8a5f5e822b6b4
retro <retrok@yandex.ru>
2022-12-19 14:13:54 +0300

    Sort massive

--- Picture.py
+++ Picture.py
@@ -28,10 +28,29 @@ class Picture:
             cv.putText(self.screenshot, name, (x, y), cv.FONT_HERSHEY_SIMPLEX, .5, color)
 
         points = []
+        namePoints = []
+        row = 0
+        column = 0
+        n = 1
         for (x, y, w, h) in rectangles:
             center_x = x + int(w / 2)
             center_y = y + int(h / 2)
+            print(n, center_x, center_y)
             points.append((center_x, center_y))
+            for (a, c) in zip(range(60, 380, 50), range(1, 8)):
+                if a <= center_x <= (a + 11):
+                    column = c
+                    print(f'{a}<x{center_x}<{a + 60}')
+                    print(f'{n} {name} строка {row}')
+                    for (b, r) in zip(range(310, 670, 50), range(1, 9)):
+                        if b <= center_y <= (b + 11):
+                            row = r
+                            print(f'{b}<y{center_y}<{b + 60}')
+                            print(f'{n} {name} столбец {column}')
+                            namePoints.append((row, column, center_x, center_y, name))
+                            print(f'{n} {name} строка {row} столбец {column}')
+                            n = n + 1
 
+        self.namePoints = namePoints
         self.points = points
         # print(f'{name}\n{points}')
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import operator
+
+import numpy as np
 from Picture import Picture
 import cv2 as cv
 from time import time, sleep
@@ -13,7 +16,6 @@ is_bot_in_action = False
 
 
 def bot_action(target):
-
     pydirectinput.moveTo(target[0], target[1])
     pydirectinput.mouseDown()
     pydirectinput.moveTo(target[0] + 50, target[1])
@@ -26,13 +28,29 @@ def bot_action(target):
 loop_time = time()
 while True:
     screenshot = wincap.get_screenshot()
-
+    mas = []
     duck = Picture('duck', (0, 255, 255), screenshot)
-    Picture('chip', (0, 0, 255), screenshot)
-    Picture('ball', (0, 255, 0), screenshot)
-    Picture('backpack', (255, 0, 0), screenshot)
-    Picture('spruce', (0, 128, 0), screenshot)
-    Picture('egg', (255, 0, 139), screenshot)
+    mas.append(duck.namePoints)
+    chip = Picture('chip', (0, 0, 255), screenshot)
+    mas.append(chip.namePoints)
+    ball = Picture('ball', (0, 255, 0), screenshot)
+    mas.append(ball.namePoints)
+    backpack = Picture('backpack', (255, 0, 0), screenshot)
+    mas.append(backpack.namePoints)
+    spruce = Picture('spruce', (0, 128, 0), screenshot)
+    mas.append(spruce.namePoints)
+    egg = Picture('egg', (255, 0, 139), screenshot)
+    mas.append(egg.namePoints)
+    print(f'mas\n{mas}')
+    mas2 = []
+    for i in mas:
+        for i2 in i:
+            mas2.append(i2)
+    mas2Sorted = sorted(mas)
+    print(f'mas2 \n {mas2Sorted} \n')
+    for x in range(0, len(mas2Sorted), 7):
+        e_c = mas2Sorted[x: 7 + x]
+        print(list(e_c))
 
     # for x in range(63, 375, 51):
     #     for y in range(189, 795, 50):
